21370932b3073c16e8968265b67f81893864eea1
Heknon <development@heknon.com>
2020-04-04 10:55:12 +0300

    added api methods to machine manager

---
+++ src/main/java/me/oriharel/machinery/exceptions/MachineNotRegisteredException.java
@@ -0,0 +1,7 @@
+package me.oriharel.machinery.exceptions;
+
+public class MachineNotRegisteredException extends Exception {
+    public MachineNotRegisteredException(String message) {
+        super(message);
+    }
+}
--- src/main/java/me/oriharel/machinery/machine/MachineManager.java
+++ src/main/java/me/oriharel/machinery/machine/MachineManager.java
@@ -1,30 +1,46 @@
 package me.oriharel.machinery.machine;
 
 import me.oriharel.machinery.Machinery;
+import me.oriharel.machinery.exceptions.MachineNotRegisteredException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 public class MachineManager {
     private final Machinery machinery;
     private MachineFactory machineFactory;
-    private List<IMachine> machines;
+    private List<Machine> machines;
+    private HashMap<UUID, PlayerMachine> playerMachines;
 
     public MachineManager(Machinery machinery) {
         this.machinery = machinery;
         this.machineFactory = new MachineFactory();
-        this.machines = new ArrayList<IMachine>();
+        this.machines = new ArrayList<Machine>();
+        this.playerMachines = new HashMap<>();
     }
 
-    public void addMachine(IMachine machine) {
+    public void registerPlayerMachine(UUID uuid, PlayerMachine playerMachine) {
+        this.playerMachines.put(uuid, playerMachine);
+    }
+
+    public PlayerMachine getPlayerMachine(UUID uuid) throws MachineNotRegisteredException {
+        PlayerMachine playerMachine = this.playerMachines.getOrDefault(uuid, null);
+        if (playerMachine == null) throw new MachineNotRegisteredException("This player machine with the uuid given has not been found in the player machine registry " +
+                ".");
+        return playerMachine;
+    }
+
+    public void addMachine(Machine machine) {
         machines.add(machine);
     }
 
-    public void removeMachine(IMachine machine) {
+    public void removeMachine(Machine machine) {
         machines.remove(machine);
     }
 
-    public List<IMachine> getMachines() {
+    public List<Machine> getMachines() {
         return machines;
     }
 
