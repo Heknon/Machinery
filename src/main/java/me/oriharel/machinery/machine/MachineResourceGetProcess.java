385122d71e278d1f7b4cab3a6e0b4018d0881012
Heknon <development@heknon.com>
2020-04-17 18:09:48 +0300

    did shit

--- src/main/java/me/oriharel/machinery/machine/MachineResourceGetProcess.java
+++ src/main/java/me/oriharel/machinery/machine/MachineResourceGetProcess.java
@@ -3,41 +3,63 @@ package me.oriharel.machinery.machine;
 import me.oriharel.machinery.Machinery;
 import me.oriharel.machinery.upgrades.AbstractUpgrade;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class MachineResourceGetProcess {
     protected List<ItemStack> itemsGained;
-    protected int timeLeft;
     protected PlayerMachine machine;
+    protected int minePeriod;
+    protected List<Material> resourceCache;
+    protected Random random;
 
     public MachineResourceGetProcess(PlayerMachine machine) {
         itemsGained = new ArrayList<>();
-        timeLeft = 100;
         this.machine = machine;
+        this.minePeriod = 20;
+        this.resourceCache = null;
+        this.random = new Random();
     }
 
     public void startProcess() {
         final List<AbstractUpgrade> upgrades = machine.getUpgrades();
-        Bukkit.getScheduler().runTaskTimerAsynchronously(Machinery.getInstance(), new BukkitRunnable() {
-            @Override
-            public void run() {
-                timeLeft--;
-                upgrades.forEach(upgrade -> upgrade.applyUpgradeModifier(MachineResourceGetProcess.this));
-                getResources();
-                if (timeLeft == 0) {
-                    cancel();
-                    upgrades.forEach(upgrade -> upgrade.applyUpgradeModifier(MachineResourceGetProcess.this));
-                }
-            }
-        }, 0, 20);
+        upgrades.forEach(upgrade -> upgrade.applyUpgradeModifier(MachineResourceGetProcess.this));
+        Bukkit.getScheduler().runTaskTimerAsynchronously(Machinery.getInstance(), () -> {
+            getResources();
+            upgrades.forEach(upgrade -> upgrade.applyUpgradeModifier(MachineResourceGetProcess.this));
+        }, 0, minePeriod);
     }
 
-    public void getResources() {
+    public void endProcess() {
 
+    }
+
+    protected void getResources() {
+        List<Material> resourceCache = this.resourceCache;
+        Random random = this.random;
+
+        if (resourceCache == null) {
+            return;
+        }
+        int chance = random.nextInt(100);
+        if (chance < 5) {
+            itemsGained.addAll(resourceCache.stream().map(mat -> new ItemStack(mat, random.nextInt(14) + 1)).collect(Collectors.toSet()));
+        } else if (chance < 50) {
+            itemsGained.addAll(resourceCache.stream().map(mat -> new ItemStack(mat, random.nextInt(9) + 1)).collect(Collectors.toSet()));
+        } else if (chance < 100) {
+            itemsGained.addAll(resourceCache.stream().map(mat -> new ItemStack(mat, random.nextInt(4) + 1)).collect(Collectors.toSet()));
+        }
+
+    }
+
+    protected void insertResources() {
+        machine.getResourcesGained().addAll(itemsGained);
     }
 
 
